cb359beb771dc681f38f084184413a9f553820a7
Mark Stopka <mark.stopka@perlur.cloud>
2019-08-19 03:51:09 +0200

    Add basic migrations

---
+++ src/database/migrations/2019_08_19_000000_add_companies_table.php
@@ -0,0 +1,31 @@
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    // Creates the companies table
+    Schema::create('companies'), function($table) {
+      $table->increments('id');
+      $table->text('company_name');
+      $table->unsignedInteger('country_id')
+      $table->unsignedInteger('chart_of_accounts_root_id')->nullable();
+      $table->timestamps();
+    )};
+  }
+
+  /**
+     * Reverse the migrations.
+     *
+     * @return void
+  */
+  public function down()
+    {
+      Schema::drop('companies');
+    }
+}
---
+++ src/database/migrations/2019_08_19_033000_add_chart_of_accounts_table.php
@@ -0,0 +1,38 @@
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChartOfAccountsTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    // Creates the chart_of_accounts table
+    Schema::create('chart_of_accounts'), function($table) {
+      $table->increments('id');
+      $table->unsignedInteger('company_id');
+      $table->unsignedInteger('account_parent_id');
+      $table->text('account_name');
+      $table->enum('account_type',
+        ['view'],
+        ['asset'],
+        ['liability'],
+        ['income'],
+        ['expense'],
+        ['other']
+      );
+    )};
+  }
+
+  /**
+     * Reverse the migrations.
+     *
+     * @return void
+  */
+  public function down()
+  {
+    Schema::drop('chart_of_accounts');
+  }
+}
