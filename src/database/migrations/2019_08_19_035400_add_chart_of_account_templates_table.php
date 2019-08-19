32c72666b573e2d84d1ef011944e1ee9527b35a5
Mark Stopka <mark.stopka@perlur.cloud>
2019-08-19 03:55:20 +0200

    Update basic migrations

---
+++ src/database/migrations/2019_08_19_035400_add_chart_of_account_templates_table.php
@@ -0,0 +1,30 @@
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChartOfAccountTemplatesTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    // Creates the chart_of_account_templates table
+    Schema::create('chart_of_account_templates'), function($table) {
+      $table->increments('id');
+      $table->text('name');
+      $table->text('template_path');
+      $table->timestamps();
+    )};
+  }
+
+  /**
+     * Reverse the migrations.
+     *
+     * @return void
+  */
+  public function down()
+  {
+    Schema::drop('chart_of_account_templates');
+  }
+}
